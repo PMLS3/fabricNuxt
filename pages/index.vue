79d9f2b67b48cb626f4ed3c1f3afa1afb19b657c
PMLS3 <62140524+PMLS3@users.noreply.github.com>
2020-08-27 12:05:03 +0200

    file upload and restore needs to be fixed

--- pages/index.vue
+++ pages/index.vue
@@ -8,6 +8,11 @@
 
     <vs-button @click="createRect()">Rect</vs-button
     ><vs-button @click="createCirc()">Circ</vs-button>
+    <vs-button @click="groupObj(group, true)">Group</vs-button>
+    <vs-button @click="groupObj(group, false)">UnGroup</vs-button>
+    <vs-button @click="restoreCanvas()">Restore</vs-button>
+    <input type="file" id="myImg" />
+
     <canvas id="canvas"></canvas>
   </div>
 </template>
@@ -27,10 +32,48 @@ export default {
       },
       currentMode: 'test',
       canvas: null,
-      colorPick: '#000000'
+      colorPick: '#000000',
+      group: {},
+      svgState: {},
+      backURL:
+        'https://media.istockphoto.com/photos/blue-abstract-background-or-texture-picture-id1138395421?k=6&m=1138395421&s=612x612&w=0&h=bJ1SRWujCgg3QWzkGPgaRiArNYohPl7-Wc4p_Fa_cyA='
     }
   },
   methods: {
+    // imgAdded(e) {
+    //   console.log('image', e)
+    //   const inputFile = document.getElementById('myImg')
+    //   console.log('input', inputFile.files)
+    // },
+    restoreCanvas() {
+      if (true) {
+        // this.canvas.clear()
+        fabric.loadSVGFromString(this.svgState.val, objects => {
+          console.log('obj', objects)
+          objects = objects.filter(o => o['xlink:href'] !== this.backURL)
+          this.canvas.add(...objects)
+          this.canvas.requestRenderAll()
+        })
+      }
+    },
+    groupObj(group, show) {
+      this.currentMode = ''
+
+      if (show) {
+        let object = this.canvas.getObjects()
+        this.group.val = new fabric.Group(object, { cornerColor: 'white' })
+        this.clearCanvas()
+        this.canvas.add(this.group.val)
+        this.canvas.requestRenderAll()
+      } else {
+        this.group.val.destroy()
+        const oldGroup = this.group.val.getObjects()
+        this.canvas.remove(this.group.val)
+        this.canvas.add(...oldGroup)
+        this.group.val = null
+        this.canvas.requestRenderAll()
+      }
+    },
     createRect() {
       const canvCenter = this.canvas.getCenter()
       console.log('canvCenter', canvCenter)
@@ -47,6 +90,16 @@ export default {
       this.canvas.add(rect)
       rect.animate('top', canvCenter.top, {
         onChange: this.canvas.renderAll.bind(this.canvas)
+      })
+      rect.on('selected', () => {
+        rect.set('fill', 'white')
+        this.canvas.renderAll()
+      })
+
+      rect.on('deselected', () => {
+        rect.set('fill', 'green')
+
+        this.canvas.renderAll()
       })
     },
     createCirc() {
@@ -86,6 +139,7 @@ export default {
       }
     },
     clearCanvas(canvas) {
+      this.svgState.val = this.canvas.toSVG()
       let vm = this
       this.canvas.getObjects().forEach(o => {
         console.log('o', o)
@@ -166,11 +220,24 @@ export default {
     }
 
     this.canvas = initCanvas('canvas')
-    setBackground(
-      'https://media.istockphoto.com/photos/blue-abstract-background-or-texture-picture-id1138395421?k=6&m=1138395421&s=612x612&w=0&h=bJ1SRWujCgg3QWzkGPgaRiArNYohPl7-Wc4p_Fa_cyA=',
-      this.canvas
-    )
+    setBackground(this.backURL, this.canvas)
     this.setPanEvents(this.canvas)
+
+    const imgAdded = e => {
+      console.log(e)
+      const inputElem = document.getElementById('myImg')
+      const file = inputElem.files[0]
+      reader.readAsDataURL(file)
+    }
+    const inputFile = document.getElementById('myImg')
+    inputFile.addEventListener('change', imgAdded)
+
+    // reader.addEventListener('load', () => {
+    //   fabric.Image.fromURL(reader.result, img => {
+    //     this.canvas.add(img)
+    //     this.canvas.requestRenderAll()
+    //   })
+    // })
   }
 }
 </script>
